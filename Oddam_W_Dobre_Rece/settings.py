8fff65619f279012d1df514e6ca445c898c4e518
Kuba <jakub.cichorski@hotmail.com>
2020-01-08 19:52:52 +0100

    added models

--- Oddam_W_Dobre_Rece/settings.py
+++ Oddam_W_Dobre_Rece/settings.py
@@ -108,9 +108,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGES = (
+    ('en-us', 'English'),
+    ('pl-PL', 'Polish'),
+)
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'pl-PL'
+
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
--- Website/models.py
+++ Website/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Institution(models.Model):
+
+    INSTITUTION_TYPE_CHOICES = (
+        (1, 'fundacja'),
+        (2, 'organizacja pozarządowa'),
+        (3, 'zbiórka lolakna'),
+    )
+
+    name = models.CharField(max_length=128)
+    description = models.TextField(null=True)
+    type = models.IntegerField(choices=INSTITUTION_TYPE_CHOICES, default=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    phone_number = models.CharField(max_length=20)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField(null=True)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, default=True)
